2dccef6a539dbc4fee49b684993ff2e2ce54363d
ATHL337 <81762936+ATHL337@users.noreply.github.com>
2023-01-17 15:47:31 -0500

    validate the ip address and hostname

--- alfredHunt.sh
+++ alfredHunt.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-while read line; do enum4linux -r $line | grep -v unknown; done < $1
+while read line; do
+    # validate the IP address or hostname
+    if [[ $line =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]] || [[ $line =~ ^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9].[a-zA-Z]{2,}$ ]]; then
+        enum4linux -r $line | grep -v unknown
+    else
+        echo "Invalid IP address or hostname: $line"
+    fi
+done < $1
