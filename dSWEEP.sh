c8c64e11f5edd600b97acd346944e615fc936c5d
ATHL337 <81762936+ATHL337@users.noreply.github.com>
2023-01-17 15:58:06 -0500

    Update dSWEEP.sh
    This script validates the input passed to it, it checks that the first parameter passed is a valid subdomain name and the second parameter passed is a valid port number. It uses the dig command with the -p option to specify the port number and the +short option to get only the necessary information, it also uses the -f option to output the result in a file.

--- dSWEEP.sh
+++ dSWEEP.sh
@@ -1,6 +1,15 @@
-#!/bin/bash -x
-subdomain=$1.0
+#!/bin/bash
 
+# validate input
+if [[ $# -ne 2 ]] || ! [[ $1 =~ ^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9].[a-zA-Z]{2,}$ ]] || ! [[ $2 =~ ^[0-9]{1,5}$ ]]; then
+    echo "Usage: $0 <subdomain> <port_number>"
+    exit 1
+fi
+
+subdomain=$1
+port=$2
+
+# perform the zone transfer
 for ip in $(seq 1 255); do 
-dig @192.168.135 $1.$ip -t axfr | grep "com"
+    dig @$subdomain $subdomain.$ip -p $port +short -t axfr -f output.txt
 done
