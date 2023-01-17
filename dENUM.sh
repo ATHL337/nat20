f7dfeba0130434db7d64c2045c10ce9669a537e4
ATHL337 <81762936+ATHL337@users.noreply.github.com>
2023-01-17 15:52:36 -0500

    Update dENUM.sh

--- dENUM.sh
+++ dENUM.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-subdomain=$1.0
 
-nmap -p 53 -T4 $subdomain/24 -oA $subdomain
-grep -B 4 open $subdomain.gnmap | grep -v "Nmap" | cut -d " " -f 2 | sort -u > $2.txt
-for ip in $(cat $2.txt); do host -t ns $ip; done
+# validate input
+if [[ $# -ne 2 ]] || ! [[ $1 =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]] || ! [[ $2 =~ ^[a-zA-Z0-9][a-zA-Z0-9-]{1,}$ ]]; then
+    echo "Usage: $0 <subnet> <output_file>"
+    exit 1
+fi
+
+subnet=$1
+output_file=$2
+
+# perform the scan
+nmap -sL -n -oN $output_file.txt $subnet/24
+
+# get the open ports
+grep -v "Status: Up" $output_file.txt | cut -d " " -f 2 | sort -u > $output_file-open.txt
+
+# get the DNS servers
+for ip in $(cat $output_file-open.txt); do host --dns-servers $ip; done
